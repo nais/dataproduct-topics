33dadc4543ac781c3ebeb9f0707de9906be8779f
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-11-10 14:37:38 +0100

    Read env vars from files, and export them

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-
 if [ "$ONPREM" = "true" ]; then
-    . /var/run/secrets/nais.io/kafka_serviceuser
+    KAFKA_USERNAME="$(cat /var/run/secrets/nais.io/kafka_serviceuser/username)"
+    KAFKA_PASSWORD="$(cat /var/run/secrets/nais.io/kafka_serviceuser/password)"
+    export KAFKA_USERNAME KAFKA_PASSWORD
 fi
 
 /app/dataproduct-topics
